62adcf94b6184f29cd91afb79f98bf0c80dd14d6
Dmitriy <diman240798@yandex.ru>
2017-12-15 15:45:56 +0300

    Second Commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,18 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.awt.peer.DialogPeer;
+import java.io.IOException;
+import java.util.Collection;
 
 public class Main extends Application {
 
@@ -12,12 +20,43 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene mainFrame = new Scene(root);
+        primaryStage.setScene(mainFrame);
         primaryStage.show();
+
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void newGame(ActionEvent event) {
+        Stage stage = new Stage();
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void ShowScores(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("Score.fxml"));
+        stage.setTitle("Score");
+
+        Scene mainFrame = new Scene(root);
+        stage.setScene(mainFrame);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void settings(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("Settings.fxml"));
+        stage.setTitle("Settings");
+        Scene mainFrame = new Scene(root);
+        stage.setScene(mainFrame);
+        stage.show();
     }
 }
